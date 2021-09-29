a214750c7482e8185dfd9dac72eb858b62d3a8e2
Papa Daco <papadaco@PapaDaco.local>
2021-09-29 16:01:14 -0400

    Jumbo images, reading list (TA:GG), and packages update

--- src/components/Main/Home/Jumbo/JumboImages.js
+++ src/components/Main/Home/Jumbo/JumboImages.js
@@ -1,71 +1,87 @@
-import acnh from '../../../../assets/images/jumbo/acnh.jpg';
+import acnh1 from '../../../../assets/images/jumbo/acnh1.jpg';
+import acnh2 from '../../../../assets/images/jumbo/acnh2.jpg';
 import botw1 from '../../../../assets/images/jumbo/botw1.jpg';
 import botw2 from '../../../../assets/images/jumbo/botw2.jpg';
 import broforce from '../../../../assets/images/jumbo/broforce.jpg';
 import celeste1 from '../../../../assets/images/jumbo/celeste1.jpg';
 import celeste2 from '../../../../assets/images/jumbo/celeste2.jpg';
+import childrenMorta from '../../../../assets/images/jumbo/childrenMorta.jpg';
 import civ from '../../../../assets/images/jumbo/civ.jpg';
 import crossing1 from '../../../../assets/images/jumbo/crossing1.jpg';
 import crossing2 from '../../../../assets/images/jumbo/crossing2.jpg';
+import deadCells from '../../../../assets/images/jumbo/deadCells.jpg';
 import escapist from '../../../../assets/images/jumbo/escapist.jpg';
 import evoland from '../../../../assets/images/jumbo/evoland.jpg';
 import fez from '../../../../assets/images/jumbo/fez.jpg';
 import gungeon1 from '../../../../assets/images/jumbo/gungeon1.jpg';
 import gungeon2 from '../../../../assets/images/jumbo/gungeon2.jpg';
+import hades from '../../../../assets/images/jumbo/hades.jpg';
 import hld from '../../../../assets/images/jumbo/hld.jpg';
 import hollow from '../../../../assets/images/jumbo/hollow.jpg';
 import hue from '../../../../assets/images/jumbo/hue.jpg';
 import humans from '../../../../assets/images/jumbo/humans.jpg';
 import katana from '../../../../assets/images/jumbo/katana.jpg';
-import legoHarry from '../../../../assets/images/jumbo/legoHarry.jpg';
+import legoHarry1 from '../../../../assets/images/jumbo/legoHarry1.jpg';
+import legoHarry2 from '../../../../assets/images/jumbo/legoHarry2.jpg';
 import lozla from '../../../../assets/images/jumbo/lozla.jpg';
 import mario from '../../../../assets/images/jumbo/mario.jpg';
 import md from '../../../../assets/images/jumbo/md.jpg';
 import ofdp from '../../../../assets/images/jumbo/ofdp.jpg';
-import ori from '../../../../assets/images/jumbo/ori.jpg';
+import ori1 from '../../../../assets/images/jumbo/ori1.jpg';
+import ori2 from '../../../../assets/images/jumbo/ori2.jpg';
 import party from '../../../../assets/images/jumbo/party.jpg';
+import phogs from '../../../../assets/images/jumbo/phogs.jpg';
 import pokemon from '../../../../assets/images/jumbo/pokemon.jpg';
 import rocket from '../../../../assets/images/jumbo/rocket.jpg';
 import smrpg from '../../../../assets/images/jumbo/smrpg.jpg';
 import spfbw from '../../../../assets/images/jumbo/spfbw.jpg';
-import spirit from '../../../../assets/images/jumbo/spirit.jpg';
+import spirit1 from '../../../../assets/images/jumbo/spirit1.jpg';
+import spirit2 from '../../../../assets/images/jumbo/spirit2.jpg';
 import ssbu from '../../../../assets/images/jumbo/ssbu.jpg';
 import terraria from '../../../../assets/images/jumbo/terraria.jpg';
 import valfaris from '../../../../assets/images/jumbo/valfaris.jpg';
 import worms from '../../../../assets/images/jumbo/worms.jpg';
 
 const JUMBOIMAGES = [
-    acnh,
+    acnh1,
+    acnh2,
     botw1,
     botw2,
     broforce,
     celeste1,
     celeste2,
+    childrenMorta,
     civ,
     crossing1,
     crossing2,
+    deadCells,
     escapist,
     evoland,
     fez,
     gungeon1,
     gungeon2,
+    hades,
     hld,
     hollow,
     hue,
     humans,
     katana,
-    legoHarry,
+    legoHarry1,
+    legoHarry2,
     lozla,
     mario,
     md,
     ofdp,
-    ori,
+    ori1,
+    ori2,
     party,
+    phogs,
     pokemon,
     rocket,
     smrpg,
     spfbw,
-    spirit,
+    spirit1,
+    spirit2,
     ssbu,
     terraria,
     valfaris,
--- src/data/readings-list.js
+++ src/data/readings-list.js
@@ -109,6 +109,7 @@ import Thinking from '../assets/images/squad/david/books/thinking.jpg';
 import Thrawn from '../assets/images/squad/david/books/thrawn.jpg';
 import ThrawnAlliances from '../assets/images/squad/david/books/thrawnAlliances.jpg';
 import ThrawnAscendancy1 from '../assets/images/squad/david/books/thrawnAscendancy1.jpg';
+import ThrawnAscendancy2 from '../assets/images/squad/david/books/thrawnAscendancy2.jpg';
 import ThrawnTeason from '../assets/images/squad/david/books/thrawnTreason.jpg';
 import TippingPoint from '../assets/images/squad/david/books/tippingPoint.jpg';
 import Vehicles from '../assets/images/squad/david/books/vehicles.jpg';
@@ -120,6 +121,13 @@ import WorldFat from '../assets/images/squad/david/books/worldFat.jpg';
 import Xenocide from '../assets/images/squad/david/books/xenocide.jpg';
 
 export const READINGSLIST = [
+    new Reading (
+        ThrawnAscendancy2.toString(), 
+        'Thrawn Ascendency: Greater Good',
+        ThrawnAscendancy2, 
+        'https://www.amazon.com/Star-Wars-Ascendancy-Greater-Trilogy-ebook/dp/B08KPJW7L9/',
+        128
+    ),
     new Reading (
         ThrawnTeason.toString(), 
         'Thrawn Treason',
