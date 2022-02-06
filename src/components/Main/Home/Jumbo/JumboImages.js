bbe6572c4c3529f78cdecd96a02d45a0a693b273
Papa Daco <papadaco@PapaDaco.local>
2022-02-05 19:35:10 -0500

    Dependencies updates and new Jumbo images

--- src/components/Main/Home/Jumbo/JumboImages.js
+++ src/components/Main/Home/Jumbo/JumboImages.js
@@ -1,8 +1,10 @@
 import acnh1 from '../../../../assets/images/jumbo/acnh1.jpg';
 import acnh2 from '../../../../assets/images/jumbo/acnh2.jpg';
+import acnh3 from '../../../../assets/images/jumbo/acnh3.jpg';
 import botw1 from '../../../../assets/images/jumbo/botw1.jpg';
 import botw2 from '../../../../assets/images/jumbo/botw2.jpg';
 import broforce from '../../../../assets/images/jumbo/broforce.jpg';
+import cadHyrule from '../../../../assets/images/jumbo/cadHyrule.jpg';
 import celeste1 from '../../../../assets/images/jumbo/celeste1.jpg';
 import celeste2 from '../../../../assets/images/jumbo/celeste2.jpg';
 import childrenMorta from '../../../../assets/images/jumbo/childrenMorta.jpg';
@@ -10,27 +12,38 @@ import civ from '../../../../assets/images/jumbo/civ.jpg';
 import crossing1 from '../../../../assets/images/jumbo/crossing1.jpg';
 import crossing2 from '../../../../assets/images/jumbo/crossing2.jpg';
 import deadCells from '../../../../assets/images/jumbo/deadCells.jpg';
+import dQ from '../../../../assets/images/jumbo/dQ.jpg';
 import escapist from '../../../../assets/images/jumbo/escapist.jpg';
 import evoland from '../../../../assets/images/jumbo/evoland.jpg';
 import fez from '../../../../assets/images/jumbo/fez.jpg';
 import gungeon1 from '../../../../assets/images/jumbo/gungeon1.jpg';
 import gungeon2 from '../../../../assets/images/jumbo/gungeon2.jpg';
-import hades from '../../../../assets/images/jumbo/hades.jpg';
+import hades1 from '../../../../assets/images/jumbo/hades1.jpg';
+import hades2 from '../../../../assets/images/jumbo/hades2.jpg';
 import hld from '../../../../assets/images/jumbo/hld.jpg';
 import hollow from '../../../../assets/images/jumbo/hollow.jpg';
 import hue from '../../../../assets/images/jumbo/hue.jpg';
 import humans from '../../../../assets/images/jumbo/humans.jpg';
+import inside from '../../../../assets/images/jumbo/inside.jpg';
 import katana from '../../../../assets/images/jumbo/katana.jpg';
 import legoHarry1 from '../../../../assets/images/jumbo/legoHarry1.jpg';
 import legoHarry2 from '../../../../assets/images/jumbo/legoHarry2.jpg';
+import legoHarry3 from '../../../../assets/images/jumbo/legoHarry3.jpg';
 import lozla from '../../../../assets/images/jumbo/lozla.jpg';
 import mario from '../../../../assets/images/jumbo/mario.jpg';
+import marioGolf1 from '../../../../assets/images/jumbo/marioGolf1.jpg';
+import marioGolf2 from '../../../../assets/images/jumbo/marioGolf2.jpg';
 import md from '../../../../assets/images/jumbo/md.jpg';
+import mh1 from '../../../../assets/images/jumbo/mh1.jpg';
+import mh2 from '../../../../assets/images/jumbo/mh2.jpg';
 import ofdp from '../../../../assets/images/jumbo/ofdp.jpg';
 import ori1 from '../../../../assets/images/jumbo/ori1.jpg';
 import ori2 from '../../../../assets/images/jumbo/ori2.jpg';
 import party from '../../../../assets/images/jumbo/party.jpg';
 import phogs from '../../../../assets/images/jumbo/phogs.jpg';
+import podRacer1 from '../../../../assets/images/jumbo/podRacer1.jpg';
+import podRacer2 from '../../../../assets/images/jumbo/podRacer2.jpg';
+import pokeArc from '../../../../assets/images/jumbo/pokeArc.jpg';
 import pokemon from '../../../../assets/images/jumbo/pokemon.jpg';
 import rocket from '../../../../assets/images/jumbo/rocket.jpg';
 import smrpg from '../../../../assets/images/jumbo/smrpg.jpg';
@@ -40,14 +53,17 @@ import spirit2 from '../../../../assets/images/jumbo/spirit2.jpg';
 import ssbu from '../../../../assets/images/jumbo/ssbu.jpg';
 import terraria from '../../../../assets/images/jumbo/terraria.jpg';
 import valfaris from '../../../../assets/images/jumbo/valfaris.jpg';
+import whatGolf from '../../../../assets/images/jumbo/whatGolf.jpg';
 import worms from '../../../../assets/images/jumbo/worms.jpg';
 
 const JUMBOIMAGES = [
     acnh1,
     acnh2,
+    acnh3,
     botw1,
     botw2,
     broforce,
+    cadHyrule,
     celeste1,
     celeste2,
     childrenMorta,
@@ -55,27 +71,38 @@ const JUMBOIMAGES = [
     crossing1,
     crossing2,
     deadCells,
+    dQ,
     escapist,
     evoland,
     fez,
     gungeon1,
     gungeon2,
-    hades,
+    hades1,
+    hades2,
     hld,
     hollow,
     hue,
     humans,
+    inside,
     katana,
     legoHarry1,
     legoHarry2,
+    legoHarry3,
     lozla,
     mario,
+    marioGolf1,
+    marioGolf2,
     md,
+    mh1,
+    mh2,
     ofdp,
     ori1,
     ori2,
     party,
     phogs,
+    podRacer1,
+    podRacer2,
+    pokeArc,
     pokemon,
     rocket,
     smrpg,
@@ -85,6 +112,7 @@ const JUMBOIMAGES = [
     ssbu,
     terraria,
     valfaris,
+    whatGolf,
     worms
 ];
 
